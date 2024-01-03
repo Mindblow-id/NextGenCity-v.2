79853f349521ca36970d18d2ed96e307733bce77
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2024-01-03 09:29:10 +0700

    Create Component

--- component/navbar.jsx
+++ component/navbar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav id="nav">
+      <h1 class="nav-logo">NGCT</h1>
+      <ul class="navigation nav-hidden">
+        <li>
+          <a href="index.html"> Home</a>
+        </li>
+        <li>
+          <a href="#about"> About</a>
+        </li>
+        <li>
+          <a href="#technology"> Technology</a>
+        </li>
+        <li>
+          <a href="#effect"> Effect</a>
+        </li>
+        <li>
+          <a href="#example"> Example</a>
+        </li>
+        <li>
+          <a href="blog.html"> Blog</a>
+        </li>
+      </ul>
+      <div id="button" class="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
