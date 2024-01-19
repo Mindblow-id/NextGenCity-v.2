ba2a00d84cb51eda76fb1198b642680598921db1
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2024-01-19 21:22:27 +0700

    add videos

--- src/pages/Technology/AI/index.jsx
+++ src/pages/Technology/AI/index.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Navbar from "../../../component/Navbar";
+import "./style.css";
 
 const AI = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    const handleVideoEnd = () => {
+      video.currentTime = 0;
+      video.play();
+    };
+    video.addEventListener("ended", handleVideoEnd);
+    return () => {
+      video.removeEventListener("ended", handleVideoEnd);
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
-      <div>
-        <img src="/gif/video (2160p)_2_.gif.crdownload" alt="" />
+      <div className="video-background">
+        <video ref={videoRef} className="video" autoPlay muted loop>
+          <source src="/mp4/Ai.mp4"/>
+        </video>
       </div>
     </>
   );
